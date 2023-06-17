0aef45b39666d3a593abf396f7517fcae0b5957d
Zabrimus <github@hannebauer.org>
2023-06-17 13:09:07 +0200

    fix scripts

--- build-setup-clang.sh
+++ build-setup-clang.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #
 # to be able to use this cross file a special build of cef is necessary and needs to be installed manually:
 # https://bitbucket.org/chromiumembedded/cef/wiki/UsingAddressSanitizer.md
