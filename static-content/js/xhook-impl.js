45e2d44a29dad90981fd269d095face58a1e9ac3
Zabrimus <github@hannebauer.org>
2023-11-23 09:05:30 +0100

    fix

---
+++ static-content/js/xhook-impl.js
@@ -0,0 +1,13 @@
+xhook.after(function (request, response) {
+    if (response.finalUrl.match(/hbbtv\.zdf\.de\/zdfm3\/dyn\/get\.php/)) {
+        try {
+            let jsonObject = JSON.parse(response.text);
+            if (jsonObject['fsk']['age'] !== undefined) {
+                delete jsonObject.fsk;
+                response.text = JSON.stringify(jsonObject);
+            }
+        } catch (err) {
+            // ignore this error
+        }
+    }
+});
